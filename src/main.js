2e3c235a39b23fc69c0eb127fda20ab10ab1ba98
Phong Vu <iphong@gmail.com>
2024-05-22 09:41:05 +0700

    copy code from other branch

--- src/main.js
+++ src/main.js
@@ -1,10 +1,49 @@
 import * as tf from '@tensorflow/tfjs'
 import { TRAINING_DATA } from 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/TrainingData/fashion-mnist.js'
-const { inputs: INPUTS } = TRAINING_DATA
-// Map output index to label
+const { inputs: INPUTS, outputs: OUTPUTS } = TRAINING_DATA
 const LOOKUP = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot' ];
 
 console.log('Loaded TensorFlow.js - version: ' + tf.version.tfjs)
+
+
+async function train() {
+	// Shuffle the two arrays in the same way so inputs still match outputs indexes.
+	tf.util.shuffleCombo(INPUTS, OUTPUTS);
+	
+  // Input feature Array is 2 dimensional.
+  const INPUTS_TENSOR = normalize(tf.tensor2d(INPUTS), 0, 255);
+	// Output feature Array is 1 dimensional.
+	const OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS, 'int32'), 10);
+
+	model = tf.sequential()
+
+	model.add(tf.layers.dense({ inputShape: [784], units: 32, activation: 'relu' }))
+	model.add(tf.layers.dense({ units: 32, activation: 'relu' }))
+	model.add(tf.layers.dense({ units: 10, activation: 'softmax' }))
+
+	model.summary()
+
+	model.compile({
+		optimizer: 'adam',
+		loss: 'categoricalCrossentropy',
+		metrics: ['accuracy']
+	})
+
+  let results = await model.fit(INPUTS_TENSOR, OUTPUTS_TENSOR, {
+		shuffle: true,
+		validationSplit: 0.2,
+		batchSize: 512,
+		epochs: 200,
+		callbacks: {
+			onEpochEnd: (epoch, logs) => {
+				console.log('epoch', epoch, '-->', logs)
+			}
+		}
+	})
+
+	INPUTS_TENSOR.dispose()
+	OUTPUTS_TENSOR.dispose()
+}
 
 function normalize(tensor, min, max) {
   const result = tf.tidy(function () {
@@ -17,7 +56,5 @@ function normalize(tensor, min, max) {
   })
   return result
 }
-// Input feature Array is 2 dimensional.
-const INPUTS_TENSOR = normalize(tf.tensor2d(INPUTS), 0, 255);
 
 INPUTS_TENSOR.print()

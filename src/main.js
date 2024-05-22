9819616a87fc08a07e9351db2f6d2ef372a7775d
Phong Vu <iphong@gmail.com>
2024-05-22 17:18:50 +0700

    add training and evaluate

--- src/main.js
+++ src/main.js
@@ -1,48 +1,86 @@
 import * as tf from '@tensorflow/tfjs'
 import { TRAINING_DATA } from 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/TrainingData/fashion-mnist.js'
 const { inputs: INPUTS, outputs: OUTPUTS } = TRAINING_DATA
-const LOOKUP = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot' ];
+const LOOKUP = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'];
 
 console.log('Loaded TensorFlow.js - version: ' + tf.version.tfjs)
 
+// Shuffle the two arrays in the same way so inputs still match outputs indexes.
+tf.util.shuffleCombo(INPUTS, OUTPUTS);
+
+// Input feature Array is 2 dimensional.
+const INPUTS_TENSOR = normalize(tf.tensor2d(INPUTS), 0, 255);
+// Output feature Array is 1 dimensional.
+const OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS, 'int32'), 10);
+
+const model = tf.sequential()
+
+model.add(tf.layers.conv2d({
+  inputShape: [28, 28, 1],
+  filters: 16,
+  kernelSize: 3, // Square filter of 3 by 3, could also be a rectangle 2 by 3
+  strides: 1, // goes through every single pixel in the image
+  padding: 'same', // to calculate pixels at the edge of the image, that doesn't have pixels around it.
+  activation: 'relu'
+}))
+model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
+model.add(tf.layers.conv2d({
+  filters: 32,
+  kernelSize: 3, // Square filter of 3 by 3, could also be a rectangle 2 by 3
+  strides: 1, // goes through every single pixel in the image
+  padding: 'same', // to calculate pixels at the edge of the image, that doesn't have pixels around it.
+  activation: 'relu'
+}))
+model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
+model.add(tf.layers.flatten());
+model.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+
+model.summary()
+
+train()
+
+const PREDICTION_ELEMENT = document.getElementById('prediction')
 
 async function train() {
-	// Shuffle the two arrays in the same way so inputs still match outputs indexes.
-	tf.util.shuffleCombo(INPUTS, OUTPUTS);
-	
-  // Input feature Array is 2 dimensional.
-  const INPUTS_TENSOR = normalize(tf.tensor2d(INPUTS), 0, 255);
-	// Output feature Array is 1 dimensional.
-	const OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS, 'int32'), 10);
+  model.compile({
+    optimizer: 'adam', // Adam changes the learning rate over time which is useful.
+    loss: 'categoricalCrossentropy', // As this is a classification problem, dont use MSE.
+    metrics: ['accuracy']
+  })
+  // reshape input from array of number to a batch of 28,28,1
+  const RESHAPED_INPUTS = INPUTS_TENSOR.reshape([INPUTS.length, 28, 28, 1]);
+  let results = await model.fit(RESHAPED_INPUTS, OUTPUTS_TENSOR, {
+    shuffle: true, // Ensure data is shuffled again before using each time.
+    validationSplit: 0.15,
+    epochs: 30, // Go over the data 30 times!
+    batchSize: 256,
+    callbacks: {
+      onEpochEnd: (epoch, logs) => {
+        console.log(epoch, '->', logs)
+      }
+    }
+  });
+  RESHAPED_INPUTS.dispose();
+  OUTPUTS_TENSOR.dispose();
+  INPUTS_TENSOR.dispose();
+  evaluate();
+}
 
-	model = tf.sequential()
-
-	model.add(tf.layers.dense({ inputShape: [784], units: 32, activation: 'relu' }))
-	model.add(tf.layers.dense({ units: 32, activation: 'relu' }))
-	model.add(tf.layers.dense({ units: 10, activation: 'softmax' }))
-
-	model.summary()
-
-	model.compile({
-		optimizer: 'adam',
-		loss: 'categoricalCrossentropy',
-		metrics: ['accuracy']
-	})
-
-  let results = await model.fit(INPUTS_TENSOR, OUTPUTS_TENSOR, {
-		shuffle: true,
-		validationSplit: 0.2,
-		batchSize: 512,
-		epochs: 200,
-		callbacks: {
-			onEpochEnd: (epoch, logs) => {
-				console.log('epoch', epoch, '-->', logs)
-			}
-		}
-	})
-
-	INPUTS_TENSOR.dispose()
-	OUTPUTS_TENSOR.dispose()
+function evaluate() {
+  const OFFSET = Math.floor((Math.random() * INPUTS.length));
+  let answer = tf.tidy(function() {
+    let newInput = normalize(tf.tensor1d(INPUTS[OFFSET]), 0, 255);
+    let output = model.predict(newInput.reshape([1, 28, 28, 1]));
+    output.print();
+    return output.squeeze().argMax();
+  })
+  answer.array().then(function (index) {
+    PREDICTION_ELEMENT.innerText = LOOKUP[index];
+    PREDICTION_ELEMENT.setAttribute('class', (index === OUTPUTS[OFFSET]) ? 'correct' : 'wrong');
+    answer.dispose();
+    // drawImage(INPUTS[OFFSET]);
+  })
 }
 
 function normalize(tensor, min, max) {
